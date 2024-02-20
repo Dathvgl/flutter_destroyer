d7659281fe899a5c17df1696057db7453a862305
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2024-02-20 20:15:00 +0700

    drawer

--- lib/drawer/drawer_header.dart
+++ lib/drawer/drawer_header.dart
@@ -2,10 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_destroyer/cubits/cultivation/cultivation_cubit.dart';
-import 'package:flutter_destroyer/cubits/userCultivation/user_cultivation_cubit.dart';
+import 'package:flutter_destroyer/cubits/user/user_cubit.dart';
 import 'package:flutter_destroyer/drawer/index.dart';
-import 'package:flutter_destroyer/enum.dart';
-import 'package:flutter_destroyer/models/user/user.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class DrawerRootHeader extends StatelessWidget {
@@ -13,14 +11,15 @@ class DrawerRootHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final inherited = DrawerRootInherited.of(context) ??
-        const DrawerRootInherited(
-          user: UserModel.empty,
-          authStatus: AuthStatus.unauthenticated,
-          child: SizedBox(),
-        );
+    final inherited = DrawerRootInherited.of(context);
 
-    if (inherited.authStatus == AuthStatus.unauthenticated) {
+    if (inherited == null) {
+      return const SizedBox();
+    }
+
+    final user = inherited.user;
+
+    if (user == null) {
       return Container(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         width: double.infinity,
@@ -36,86 +35,95 @@ class DrawerRootHeader extends StatelessWidget {
           ),
         ),
       );
-    } else {
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 12),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: SizedBox(
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 35,
-                      foregroundImage: inherited.user.photo == null
-                          ? null
-                          : CachedNetworkImageProvider(inherited.user.photo!),
-                    ),
-                    const SizedBox(width: 12),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          inherited.user.name ?? "empty",
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SizedBox(
+              width: double.infinity,
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 35,
+                    foregroundImage: user.thumnail == null
+                        ? null
+                        : CachedNetworkImageProvider(user.thumnail!),
+                  ),
+                  const SizedBox(width: 12),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        user.name ?? "empty",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-                        BlocBuilder<UserCultivationCubit, UserCultivationState>(
-                          builder: (context, state) {
-                            return Text(
-                              state.cultivation.xungHo,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                fontSize: 16,
-                              ),
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                      BlocBuilder<UserCubit, UserState>(
+                        builder: (context, state) {
+                          if (state.cultivation == null) {
+                            return const SizedBox();
+                          }
+
+                          return Text(
+                            state.cultivation!.xungHo,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            BlocBuilder<CultivationCubit, CultivationState>(
-              builder: (context, cultivationState) {
-                final canhGioi = cultivationState.tuLuyen.canhGioi;
-                return BlocBuilder<UserCultivationCubit, UserCultivationState>(
-                  builder: (context, userState) {
-                    double percent = 0;
+          ),
+          BlocBuilder<CultivationCubit, CultivationState>(
+            builder: (context, cultivationState) {
+              final canhGioi = cultivationState.tuLuyen.canhGioi;
 
-                    final current =
-                        canhGioi.get(userState.cultivation.idCanhGioi);
-                    final next = canhGioi.get(current?.nextId);
+              return BlocBuilder<UserCubit, UserState>(
+                builder: (context, userState) {
+                  if (userState.cultivation == null) {
+                    return const SizedBox();
+                  }
 
-                    if (current != null && next != null) {
-                      final need = userState.cultivation.tuVi - current.tuVi;
-                      final total = next.tuVi - current.tuVi;
-                      percent = need / total;
-                    }
+                  double percent = 0;
 
-                    return Padding(
-                      padding: const EdgeInsets.only(top: 16, bottom: 8),
-                      child: LinearPercentIndicator(
-                        percent: percent <= 1 ? percent : 0,
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ],
-        ),
-      );
-    }
+                  final current =
+                      canhGioi.get(userState.cultivation!.idCanhGioi);
+                  final next = canhGioi.get(current?.nextId);
+
+                  if (current != null && next != null) {
+                    final need = userState.cultivation!.tuVi - current.tuVi;
+                    final total = next.tuVi - current.tuVi;
+                    percent = need / total;
+                  }
+
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 8),
+                    child: LinearPercentIndicator(
+                      percent: percent <= 1 ? percent : 0,
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/drawer/index.dart
+++ lib/drawer/index.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_destroyer/blocs/auth/auth_bloc.dart';
 import 'package:flutter_destroyer/cubits/theme/theme_cubit.dart';
+import 'package:flutter_destroyer/cubits/user/user_cubit.dart';
 import 'package:flutter_destroyer/drawer/drawer_content.dart';
 import 'package:flutter_destroyer/drawer/drawer_footer.dart';
 import 'package:flutter_destroyer/drawer/drawer_header.dart';
-import 'package:flutter_destroyer/enum.dart';
 import 'package:flutter_destroyer/models/user/user.dart';
 
 class DrawerRootInherited extends InheritedWidget {
-  final UserModel user;
-  final AuthStatus authStatus;
+  final UserModel? user;
 
   const DrawerRootInherited({
     super.key,
     required this.user,
-    required this.authStatus,
     required super.child,
   });
 
@@ -25,7 +22,7 @@ class DrawerRootInherited extends InheritedWidget {
 
   @override
   bool updateShouldNotify(DrawerRootInherited oldWidget) {
-    return authStatus != oldWidget.authStatus;
+    return user != oldWidget.user;
   }
 }
 
@@ -35,34 +32,36 @@ class DrawerRoot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: BlocBuilder<AuthBloc, AuthState>(
-        builder: (context, state) {
-          state.user;
-          return DrawerRootInherited(
-            user: state.status == AuthStatus.authenticated
-                ? state.user
-                : UserModel.empty,
-            authStatus: state.status,
-            child: BlocBuilder<ThemeCubit, ThemeState>(
-              builder: (context, state) {
-                return Container(
-                  color: state.useTheme.scaffoldBackgroundColor,
-                  child: const Column(
-                    children: [
-                      DrawerRootHeader(),
-                      DrawerRootContent(),
-                      Divider(
-                        height: 0,
-                        thickness: 1,
-                      ),
-                      DrawerRootFooter(),
-                    ],
-                  ),
-                );
-              },
-            ),
-          );
-        },
+      child: SafeArea(
+        child: BlocBuilder<UserCubit, UserState>(
+          // buildWhen: (previous, current) {
+          //   print("hahaha ${previous.user} ||| ${current.user}");
+          //   return previous.user != current.user;
+          // },
+          builder: (context, state) {
+            return DrawerRootInherited(
+              user: state.user,
+              child: BlocBuilder<ThemeCubit, ThemeState>(
+                builder: (context, state) {
+                  return Container(
+                    color: state.useTheme.scaffoldBackgroundColor,
+                    child: const Column(
+                      children: [
+                        DrawerRootHeader(),
+                        DrawerRootContent(),
+                        Divider(
+                          height: 0,
+                          thickness: 1,
+                        ),
+                        DrawerRootFooter(),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
