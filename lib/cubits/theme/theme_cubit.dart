2acacccc572b0477eaa0f8ac3fbea73ff5e67741
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2024-02-27 11:53:30 +0700

    hive theme

--- lib/cubits/theme/theme_cubit.dart
+++ lib/cubits/theme/theme_cubit.dart
@@ -3,6 +3,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 part 'theme_state.dart';
 
@@ -10,10 +11,19 @@ final _lightTheme = ThemeData.light(useMaterial3: true);
 final _darkTheme = ThemeData.dark(useMaterial3: true);
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit() : super(ThemeInitial(theme: false, useTheme: _lightTheme));
+  ThemeCubit({
+    bool theme = false,
+  }) : super(ThemeInitial(
+          theme: theme,
+          useTheme: theme ? _darkTheme : _lightTheme,
+        ));
 
-  void listen(bool theme) => emit(ThemeInitial(
-        theme: theme,
-        useTheme: theme ? _darkTheme : _lightTheme,
-      ));
+  Future<void> listen(bool theme) async {
+    await Hive.box("settings").put("theme", theme);
+
+    emit(ThemeInitial(
+      theme: theme,
+      useTheme: theme ? _darkTheme : _lightTheme,
+    ));
+  }
 }
